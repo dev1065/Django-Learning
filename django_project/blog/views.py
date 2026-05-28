b8a926c64da567911d7d68427bbb3ddea7dd3d13
Dev Sharma <devsharma1065@gmail.com>
2026-05-28 21:52:11 +0530

    added one more template about page

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
@@ -7,4 +6,4 @@ def home(request):
 
 
 def about(request):
-    return HttpResponse('<h1> Home About </h1>')
+    return render(request, 'blog/about.html')
