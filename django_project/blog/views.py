751208dae84b1e42023c76bfd607b90033317641
Dev Sharma <devsharma1065@gmail.com>
2026-05-28 21:29:42 +0530

    learning about templates and routing them

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
-    return HttpResponse('<h1> Home Page </h1>')
+    return render(request, 'blog/home.html')
+
+
 def about(request):
     return HttpResponse('<h1> Home About </h1>')
